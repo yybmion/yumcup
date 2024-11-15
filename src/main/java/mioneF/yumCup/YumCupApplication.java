750350ffaeaa010b8b73f8adc1a25851028e7393
yybmion <yyb400@ajou.ac.kr>
2024-11-15 21:11:04 +0900

    프로젝트 초기 세팅-백엔드

---
+++ src/main/java/mioneF/yumCup/YumCupApplication.java
@@ -0,0 +1,13 @@
+package mioneF.yumCup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumCupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mioneF/yumCup/YumCupApplicationTests.java
@@ -0,0 +1,13 @@
+package mioneF.yumCup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
